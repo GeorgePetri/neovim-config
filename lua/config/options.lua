c031df08a5ce46309f7908d7f684c003a08d22f3
George Petri <t8_8t@icloud.com>
2022-09-08 15:28:33 +0300

    use lua for options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,24 +1,26 @@
---todo lua
+local options = {
+    completeopt = { "menu", "menuone", "noselect" }, --autocomplete options: show menu, show menu even if just one entry, do not autoselect
+    mouse = "a", --enable mouse
+    expandtab = true, --tab inserts space characters
+    tabstop = 4, --tab equals 4 spaces
+    shiftwidth = 4, --set identation to the same values as tabstop
+    number = true, --show absolute line numbers
+    ignorecase = true, --case insensitive search
+    smartcase = true, --case insensitive search just when searching all lower letters
+    --TODO set diffopt+=vertical
+    writebackup = false, --don't create backup files
+    --TODO shortmess+=c
+    signcolumn = "yes", --add a column to the left of the numbers column for signs from lsp for example
+    updatetime = 520, --update time in milliseconds
+    undofile = true --persist undo tree
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
+
 vim.cmd [[
-set completeopt=menu,menuone,noselect "autocomplete options: show menu, show menu even if just one entry, do not autoselect
-set mouse=a "enable mouse
-set expandtab "tab inserts space characters
-set tabstop=4 "tab equals 4 spaces
-set shiftwidth=4 "set identation to the same values as tabstop
-set number "show absolute line numbers
-set ignorecase "case insensitive search
-set smartcase "case insensitive search just when searching all lower letters
-"TODO set diffopt+=vertical
-set nowritebackup "don't create backup files
-"TODO shortmess+=c
-set signcolumn=yes "add a column to the left of the numbers column for signs from lsp for example
-set updatetime=520 "update time in milliseconds
-set undofile "persist undo tree
-
-let mapleader = " " "leader is space
-nnoremap <leader>v :e $MYVIMRC<CR>
-
-"theme
-set background=light
-colorscheme PaperColor
+	set background=light
+	colorscheme PaperColor
 ]]
+
