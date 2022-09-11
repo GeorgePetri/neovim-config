df2399015679c354c42eade44471af9fe6aa7eeb
George Petri <t8_8t@icloud.com>
2022-09-11 19:51:20 +0300

    add git keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,4 +7,10 @@ nnoremap <leader>v :e $MYVIMRC<CR>
 nnoremap <leader>fg <cmd>lua require('fzf-lua').grep()<CR>
 vnoremap <leader>fg <cmd>lua require('fzf-lua').grep_visual()<CR>
 noremap <leader>ff <cmd>lua require('fzf-lua').files()<CR>
+
+nnoremap <leader>vv <cmd>lua require('fzf-lua').git_status()<CR>
+nnoremap <leader>vc <cmd>lua require('fzf-lua').git_commits()<CR>
+nnoremap <leader>vs <cmd>lua require('fzf-lua').git_stash()<CR>
+nnoremap <leader>vb <cmd>lua require('fzf-lua').git_branches()<CR>
+nnoremap <leader>vf <cmd>lua require('fzf-lua').git_files()<CR>
 ]]
