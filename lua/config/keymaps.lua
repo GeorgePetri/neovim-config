b622b510984d1bc2b27cf97e0aed82252c7fdf0e
George Petri <t8_8t@icloud.com>
2022-09-12 15:38:10 +0300

    keymap in lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,16 +1,14 @@
---todo lua
---TODO check rg, fzf, fg configs and get a good setup
-vim.cmd [[
-let mapleader = " " "leader is space
-nnoremap <leader>v :e $MYVIMRC<CR>
+function keymap (mode, lhs, rhs)
+    vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true })
+end
 
-nnoremap <leader>fg <cmd>lua require('fzf-lua').grep()<CR>
-vnoremap <leader>fg <cmd>lua require('fzf-lua').grep_visual()<CR>
-noremap <leader>ff <cmd>lua require('fzf-lua').files()<CR>
+vim.g.mapleader = " "
+keymap("n", "<leader>fg", "<cmd>lua require('fzf-lua').grep()<CR>")
+keymap("v", "<leader>fg", "<cmd>lua require('fzf-lua').grep_visual()<CR>")
+keymap("n", "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>")
 
-nnoremap <leader>vv <cmd>lua require('fzf-lua').git_status()<CR>
-nnoremap <leader>vc <cmd>lua require('fzf-lua').git_commits()<CR>
-nnoremap <leader>vs <cmd>lua require('fzf-lua').git_stash()<CR>
-nnoremap <leader>vb <cmd>lua require('fzf-lua').git_branches()<CR>
-nnoremap <leader>vf <cmd>lua require('fzf-lua').git_files()<CR>
-]]
+keymap("n", "<leader>vv", "<cmd>lua require('fzf-lua').git_status()<CR>")
+keymap("n", "<leader>vc", "<cmd>lua require('fzf-lua').git_commits()<CR>")
+keymap("n", "<leader>vs", "<cmd>lua require('fzf-lua').git_stash()<CR>")
+keymap("n", "<leader>vb", "<cmd>lua require('fzf-lua').git_branches()<CR>")
+keymap("n", "<leader>vf", "<cmd>lua require('fzf-lua').git_files()<CR>")
