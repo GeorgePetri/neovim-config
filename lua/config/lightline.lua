9c70b74532cc7d9e54302da28287ab8894b4819f
George Petri <t8_8t@icloud.com>
2022-09-20 21:53:30 +0300

    lua lightline

--- lua/config/lightline.lua
+++ lua/config/lightline.lua
@@ -1,14 +1,7 @@
---todo write in lua
-vim.cmd [[
-let g:lightline = {
-        \ 'colorscheme': 'PaperColor',
-        \ 'active': {
-        \   'left': [ [ 'mode', 'paste' ],
-        \       [ 'gitbranch', 'readonly', 'filename', 'modified' ] ]
-        \ },
-        \ 'component_function': {
-        \   'gitbranch': 'gitbranch#name'
-        \ },
-        \ }
-]]
+vim.g.lightline = {
+    colorscheme = 'PaperColor',
+    active = {
+        left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'filename', 'modified' } } },
+        component_function = { gitbranch = 'gitbranch#name' },
+}
 
