d91f9bca2fe0217be7970544d9f4cea2f0985c7d
George Petri <t8_8t@icloud.com>
2022-09-23 22:35:15 +0300

    use all lua

---
+++ init.lua
@@ -0,0 +1,8 @@
+require'config.plugins'
+require'config.cmp'
+require'config.fzf'
+require'config.keymaps'
+require'config.lightline'
+require'config.lsp'
+require'config.options'
+require'config.treesitter'
