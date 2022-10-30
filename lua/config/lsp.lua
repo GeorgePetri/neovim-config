b038de228fb1e8ce9fa6d1ef53cad046988e4689
George Petri <t8_8t@icloud.com>
2022-10-30 17:43:04 +0200

    update capabilities -> default capabilities

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,5 +1,5 @@
 local opts = { noremap=true, silent=true }
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 function bind_common(client, bufnr)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
